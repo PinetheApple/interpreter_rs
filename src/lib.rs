51f5a7d51deabf962ee7575839cbc665d63afa88
PinetheApple <pinespace889@gmail.com>
2024-11-09 22:06:35 +0530

    equality and assignment

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,27 @@
 pub fn tokenize(file_contents: String) -> i32 {
     let mut status_code: i32 = 0;
+    let mut tokens: Vec<&str> = vec![];
     for (i, line) in file_contents.lines().enumerate() {
         for c in line.chars() {
             match c {
-                '(' => println!("LEFT_PAREN ( null"),
-                ')' => println!("RIGHT_PAREN ) null"),
-                '{' => println!("LEFT_BRACE {{ null"),
-                '}' => println!("RIGHT_BRACE }} null"),
-                '*' => println!("STAR * null"),
-                '.' => println!("DOT . null"),
-                ',' => println!("COMMA , null"),
-                '+' => println!("PLUS + null"),
-                '-' => println!("MINUS - null"),
-                ';' => println!("SEMICOLON ; null"),
+                '(' => tokens.push("LEFT_PAREN ( null"),
+                ')' => tokens.push("RIGHT_PAREN ) null"),
+                '{' => tokens.push("LEFT_BRACE {{ null"),
+                '}' => tokens.push("RIGHT_BRACE }} null"),
+                '*' => tokens.push("STAR * null"),
+                '.' => tokens.push("DOT . null"),
+                ',' => tokens.push("COMMA , null"),
+                '+' => tokens.push("PLUS + null"),
+                '-' => tokens.push("MINUS - null"),
+                ';' => tokens.push("SEMICOLON ; null"),
+                '=' => {
+                    let prev = tokens.pop().unwrap();
+                    if prev == "EQUAL = null" {
+                        tokens.push("EQUAL_EQUAL == null");
+                    } else {
+                        tokens.push("EQUAL = null");
+                    }
+                }
                 a => {
                     status_code = 65;
                     eprintln!("[line {}] Error: Unexpected character: {}", i + 1, a)
@@ -20,6 +29,6 @@ pub fn tokenize(file_contents: String) -> i32 {
             }
         }
     }
-    println!("EOF  null");
+    tokens.push("EOF  null");
     status_code
 }
