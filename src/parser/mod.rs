99042f4371edd9cb680a7f63f3c0535f2879da83
PinetheApple <pinespace889@gmail.com>
2024-11-14 19:34:10 +0530

    operations

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -17,12 +17,7 @@ where
                 }
                 TokenType::NUMBER | TokenType::STRING => parsed_output.push(token.literal.clone()),
                 TokenType::LEFT_PAREN => {
-                    let mut group_str = String::new();
-                    let (parsed_group, group_status_code) = parse(token_iter, true);
-                    for parsed_line in parsed_group {
-                        group_str = format!("{}{}", group_str, parsed_line);
-                    }
-
+                    let (group_str, group_status_code) = parse_group(token_iter);
                     if group_status_code != 0 {
                         status_code = group_status_code;
                     }
@@ -36,18 +31,36 @@ where
                         status_code = 65;
                     }
                 }
-                TokenType::MINUS | TokenType::BANG => {
-                    let mut group_str = token.lexeme + " ";
-                    let (parsed_group, group_status_code) = parse(token_iter, is_group);
-                    for parsed_line in parsed_group {
-                        group_str = format!("{}{}", group_str, parsed_line);
-                    }
-
+                TokenType::MINUS => {
+                    let (unary_str, group_status_code) =
+                        parse_unary_operations(token_iter, is_group);
                     if group_status_code != 0 {
                         status_code = group_status_code;
                     }
 
-                    parsed_output.push(format!("({})", group_str));
+                    parsed_output.push(format!("(- {})", unary_str));
+                }
+                TokenType::BANG => {
+                    let (unary_str, group_status_code) =
+                        parse_unary_operations(token_iter, is_group);
+                    if group_status_code != 0 {
+                        status_code = group_status_code;
+                    }
+
+                    parsed_output.push(format!("(! {})", unary_str));
+                }
+                TokenType::SLASH | TokenType::STAR => {
+                    if let Some(prev_val) = parsed_output.pop() {
+                        let (op_str, group_status_code) =
+                            parse_operations(token_iter, token.lexeme, prev_val);
+                        if group_status_code != 0 {
+                            status_code = group_status_code;
+                        }
+
+                        parsed_output.push(op_str);
+                    } else {
+                        status_code = 65;
+                    }
                 }
                 _ => {
                     status_code = 65;
@@ -61,4 +74,55 @@ where
     }
 
     (parsed_output, status_code)
+}
+
+fn parse_group<I>(token_iter: &mut I) -> (String, i32)
+where
+    I: Iterator<Item = Token>,
+{
+    let mut status_code = 0;
+    let mut group_str = String::new();
+    let (parsed_group, group_status_code) = parse(token_iter, true);
+    for parsed_line in parsed_group {
+        group_str = format!("{}{}", group_str, parsed_line);
+    }
+
+    if group_status_code != 0 {
+        status_code = group_status_code;
+    }
+
+    (group_str, status_code)
+}
+
+fn parse_unary_operations<I>(token_iter: &mut I, is_group: bool) -> (String, i32)
+where
+    I: Iterator<Item = Token>,
+{
+    let mut status_code = 0;
+    let mut unary_str = String::new();
+    let (parsed_group, group_status_code) = parse(token_iter, is_group);
+    for parsed_line in parsed_group {
+        unary_str = format!("{}{}", unary_str, parsed_line);
+    }
+
+    if group_status_code != 0 {
+        status_code = group_status_code;
+    }
+
+    (unary_str, status_code)
+}
+
+fn parse_operations<I>(token_iter: &mut I, operator: String, prev_val: String) -> (String, i32)
+where
+    I: Iterator<Item = Token>,
+{
+    let mut status_code = 0;
+    let mut op_str = String::new();
+    if let Some(token) = token_iter.next() {
+        op_str = format!("({} {} {})", operator, prev_val, token.literal);
+    } else {
+        status_code = 65;
+    }
+
+    (op_str, status_code)
 }
